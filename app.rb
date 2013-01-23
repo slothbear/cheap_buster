5d80ea20b6519b02b1e975f3464e67201bb77db8
Paul Morganthall <paul@morganthall.com>
2013-01-22 22:18:49 -0500

    Move #predictions to Nextbus module.

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require_relative 'nextbus'
+include Nextbus
 
 get '/' do
   'To see predictions for a stop, add the stop identifier
@@ -8,8 +10,4 @@ end
 get '/:stop_id' do
   stop_id = params[:stop_id]
   "Predictions for stop #{stop_id}: #{predictions(stop_id)}"
-end
-
-def predictions(stop_id)
-  "10, 24, 39"
 end
---
+++ nextbus.rb
@@ -0,0 +1,5 @@
+module Nextbus
+  def predictions(stop_id)
+    "10, 24, 39"
+  end
+end
