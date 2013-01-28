1745313294cb0f22232c281cf755bbb8f68f5d70
Paul Morganthall <paul@morganthall.com>
2013-01-27 19:49:03 -0500

    Better instructions.

--- app.rb
+++ app.rb
@@ -3,8 +3,19 @@ require_relative 'nextbus'
 include Nextbus
 
 get '/' do
-  'To see predictions for a stop, add the stop identifier
-     to the URL. For instance: cheapbuster.herokuapp.com/00072'
+  '<p>To see predictions for a stop, add the stop identifier
+  to the URL. For instance:
+  <pre>cheapbuster.herokuapp.com/00072</pre>
+  </p>
+
+  <p>Look up MBTA stop identifiers at
+  <a href="http://www.nextbus.com/predictor/stopSelector.jsp?a=mbta">
+  NextBus</a></p>
+
+  <p>Cheap Buster has only been tested with the MBTA so far.
+  If you would like to use it with a different transit
+  system, let me know
+  or send a pull request.</p>'
 end
 
 get '/:stop_id' do
