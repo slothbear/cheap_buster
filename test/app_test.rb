7bb2732f37984b92e19499c96ef26b2fb7634d8c
Paul Morganthall <paul@morganthall.com>
2013-03-18 18:40:06 -0400

    no GETs in this API

--- test/app_test.rb
+++ test/app_test.rb
@@ -2,13 +2,6 @@ require_relative 'test_helper'
 
 class AppTest < MiniTest::Unit::TestCase
 
-  def test_stop_predictions
-    VCR.use_cassette('predictions') do
-      get '/00072'
-      assert_match /10, 24, 39/, last_response.body
-    end
-  end
-
   def test_record_predictions
     VCR.use_cassette('predictions') do
      response = HTTParty.get('http://webservices.nextbus.com/service/publicXMLFeed',
