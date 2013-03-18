27b258ec8b4dc743e6c3ac4c6898f5f37fa5d4dd
Paul Morganthall <paul@morganthall.com>
2013-03-18 18:58:18 -0400

    remove inaccurate home page junk

--- app.rb
+++ app.rb
@@ -3,19 +3,10 @@ require_relative 'nextbus'
 include Nextbus
 
 get '/' do
-  '<p>To see predictions for a stop, add the stop identifier
-  to the URL. For instance:
-  <pre>cheapbuster.herokuapp.com/00072</pre>
-  </p>
-
-  <p>Look up MBTA stop identifiers at
-  <a href="http://www.nextbus.com/predictor/stopSelector.jsp?a=mbta">
-  NextBus</a>.</p>
-
-  <p>Cheap Buster has only been tested with the MBTA so far.
-  If you would like to use it with a different transit
-  system, let me know
-  or send a pull request.</p>'
+  '<p>
+    Read more about
+    <a href="http://github.com/slothbear/cheap_buster">Cheap Buster</a>
+  </p>'
 end
 
 get '/:stop_id' do
