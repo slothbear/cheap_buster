27f9fa4678260ae10dd0580cc0299c7fceda1b56
Paul Morganthall <paul@morganthall.com>
2013-03-18 20:21:35 -0400

    get rid of last GET

--- app.rb
+++ app.rb
@@ -1,15 +1,8 @@
 require 'sinatra'
-require_relative 'nextbus'
-include Nextbus
 
 get '/' do
   '<p>
     Read more about
     <a href="http://github.com/slothbear/cheap_buster">Cheap Buster</a>
   </p>'
-end
-
-get '/:stop_id' do
-  stop_id = params[:stop_id]
-  "Predictions for stop #{stop_id}: #{predictions(stop_id)}"
 end
