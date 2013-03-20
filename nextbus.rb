191bf3d65bb3b97c46d6829d5aa3f1e689125a13
Paul Morganthall <paul@morganthall.com>
2013-03-19 23:02:40 -0400

    get predictions in minutes

--- nextbus.rb
+++ nextbus.rb
@@ -1,16 +1,15 @@
 module Nextbus
-  def predictions(stop_id)
-    response = query_nextbus(stop_id)
-    response['predictions'].count
+  def self.predictions(stop_id)
+    query_nextbus(stop_id).collect do |prediction|
+      prediction['minutes']
+    end
   end
 
-  def query_nextbus(stop_id)
+  def self.query_nextbus(stop_id)
     base_uri = 'http://webservices.nextbus.com/service/publicXMLFeed'
-    query = { :command => 'predictions',
-      :a => 'mbta',
-      :stopId => "#{stop_id}"
-      }
+    query = { :command => 'predictions', :a => 'mbta', :stopId => "#{stop_id}" }
+
     response = HTTParty.get(base_uri, query: query)
-    response.parsed_response['body']
+    response.parsed_response['body']['predictions']['direction']['prediction']
   end
 end
--- test/app_test.rb
+++ test/app_test.rb
@@ -1,5 +1,7 @@
 require_relative 'test_helper'
+require_relative '../nextbus.rb'
 
+BENTON_SQUARE_ID = '02108'
 BENTON_SQUARE = { :command => 'predictions', :a => 'mbta', :stopId => '02108' }
 
 class AppTest < MiniTest::Unit::TestCase
@@ -8,6 +10,12 @@ class AppTest < MiniTest::Unit::TestCase
     VCR.use_cassette('benton_square') do
      response = HTTParty.get(NEXTBUS, :query => BENTON_SQUARE)
      assert_match /Benton Square/, response.body
+    end
+  end
+
+  def test_benton_square_predictions
+    VCR.use_cassette('benton_square') do
+      assert_equal %w[11 26 41 56 71], Nextbus.predictions(BENTON_SQUARE_ID)
     end
   end
 
