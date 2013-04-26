11d4448d6e055873b9a1f4a73e2276680500bcef
Paul Morganthall <paul@morganthall.com>
2013-04-26 09:34:21 -0400

    better name for QUERY

--- test/app_test.rb
+++ test/app_test.rb
@@ -3,13 +3,13 @@ require_relative '../nextbus.rb'
 require 'mail'
 
 BENTON_SQUARE_ID = '02108'
-BENTON_SQUARE = { :command => 'predictions', :a => 'mbta', :stopId => '02108' }
+BENTON_SQUARE_QUERY = { :command => 'predictions', :a => 'mbta', :stopId => '02108' }
 
 class AppTest < MiniTest::Unit::TestCase
 
   def test_shambling_skeleton
     VCR.use_cassette('benton_square') do
-     response = HTTParty.get(NEXTBUS, :query => BENTON_SQUARE)
+     response = HTTParty.get(NEXTBUS, :query => BENTON_SQUARE_QUERY)
      assert_match /Benton Square/, response.body
     end
   end
